83bc0986ae4c9d39e5a619cd5630ea508a8207ad
Isaac <coldsud@gmail.com>
2025-04-05 19:40:23 -0400

    cyse 270 bash script

---
+++ bash/CYSE270Lab8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+num = input("Enter Number for evalutatoin")
+
+if [ $num -eq 10 ]
+
+then
+
+	echo "Input number is greater than 10"
+
+else
+
+	echo "Input number is not greater than 10"
+
+fi
+echo "Thanks for checking"
+
